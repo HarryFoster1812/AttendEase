1c7fe81164c2e5e3f479f1b26de48d8b2a060567
r49769hf <harry.foster-2@student.manchester.ac.uk>
2025-03-21 17:03:37 +0000

    changed db.php

--- php/db.php
+++ php/db.php
@@ -5,22 +5,16 @@ require_once 'classes/Database.php';
 
 $port          = "3306";
 
-// $database_host = "***REMOVED***";
-// $database_user = "***REMOVED***";
-// $database_pass = "***REMOVED***";
-// $database_name = "***REMOVED***";
+$database_host = "***REMOVED***";
+$database_user = "***REMOVED***";
+$database_pass = "***REMOVED***";
+$database_name = "***REMOVED***";
 
-$database_host = "localhost";
-$database_user = "root";
-$database_pass = "new_password";
-$database_name = "AttendEase";
+// $database_host = "localhost";
+// $database_user = "root";
+// $database_pass = "password";
+// $database_name = "AttendEase";
 
-
-
-//$database_host = "sql213.infinityfree.com";
-//$database_user = "***REMOVED***";
-//$database_pass = "***REMOVED***";
-//$database_name = "***REMOVED***_attend_ease";
 
 // try to create a mysql database connection using a new PDO object
 // by specifying the database type, host, dbname - then username and pw.
